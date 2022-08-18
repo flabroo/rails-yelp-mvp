e7960a9008c1751b88956f0ff139b71344ce6b48
Fairul Izwan <izwanrul11@gmail.com>
2022-08-18 15:07:55 +0800

    assignment completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Superhero.name,
+    address: Faker::Address.street_address,
+    phone_number: rand(100000..999999).to_s,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  restaurant.save!
+end
